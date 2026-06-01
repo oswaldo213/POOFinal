766f583593dc5da3cae60c7fa0d5d7c775add833
oswaldo213 <cesar3dmendoza@gmail.com>
2026-05-31 22:40:18 -0700

    estructura basica de programa bin,src y lib

--- practica/src/ABC_Menu.java
+++ practica/src/ABC_Menu.java
@@ -1,21 +1,48 @@
+import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.sql.*;
 import javax.swing.*;
-public class ABC_Menu extends JFrame {
+
+public class ABC_Menu extends JFrame implements ActionListener {
+
     JFrame Menu = new JFrame();
     JMenuBar menubar = new JMenuBar();
-    JMenu professor = new JMenu("Professor");
+    JMenu subMenu = new JMenu("Menu");
+    JMenuItem professor = new JMenuItem("Professors");
 
-    public ABC_Menu(){
-        Menu = new JFrame();
-        menubar = new JMenuBar();
-        menubar.add(professor);
+    Connection con;
 
-        Menu.setSize(400,200);
+    public ABC_Menu() {
+        subMenu.add(professor);
+        menubar.add(subMenu);
+        Menu.setSize(600, 300);
         Menu.setVisible(true);
         Menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Menu.setLayout(none);
+        Menu.setJMenuBar(menubar);
 
-        Menu.add(menubar);
+        Menu.setLayout(null);
+        popo();
+    }
 
+    public void popo() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            String url =
+                "jdbc:mariadb://localhost:3306/Practica?user=oswaldo&password=1234";
+            con = DriverManager.getConnection(url);
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "hey como que no hay driver");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "jsjjs no te jalo wey");
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == professor) {
+        }
     }
 }
+//javac -cp "lib/*" -d bin src/*.java
+//java -cp "lib/*:bin" Main
