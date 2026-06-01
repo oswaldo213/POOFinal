4f0fdfef78f72d35ee0d63a3c02b446a1c5454d3
oswaldo213 <cesar3dmendoza@gmail.com>
2026-05-31 20:46:55 -0700

    zed

--- practica/src/ABC_Menu.java
+++ practica/src/ABC_Menu.java
@@ -1,5 +1,21 @@
-public class ABC_Menu {
+import javax.swing.*;
+public class ABC_Menu extends JFrame {
+    JFrame Menu = new JFrame();
+    JMenuBar menubar = new JMenuBar();
+    JMenu professor = new JMenu("Professor");
+
     public ABC_Menu(){
+        Menu = new JFrame();
+        menubar = new JMenuBar();
+        menubar.add(professor);
+
+        Menu.setSize(400,200);
+        Menu.setVisible(true);
+        Menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Menu.setLayout(none);
+
+        Menu.add(menubar);
 
     }
 }
